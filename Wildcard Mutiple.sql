599bdeda68bc8a797fb7a58bcd3f5ec7a874de7f
Bryant Truong <Benten1350@yahoo.com>
2018-04-17 13:39:17 -0400

    Update and rename Wildcard Mutiple.sql to Wildcard Starting Multiple.sql

--- Wildcard Mutiple.sql
+++
@@ -1,2 +0,0 @@
-select * from employees
-where FirstName like '[afd]%';
---
+++ Wildcard Starting Multiple.sql
@@ -0,0 +1,5 @@
+select * from employees
+where FirstName like '[afd]%';
+
+select * from employees
+where FirstName like '[a-d]%';
