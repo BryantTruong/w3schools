560beaa7b519956a42eb4c1c88ca7f6ff2967f42
Bryant Truong <Benten1350@yahoo.com>
2018-04-16 22:13:16 -0400

    Update Insert Into.sql

--- Insert Into.sql
+++ Insert Into.sql
@@ -3,3 +3,8 @@ values ('Personal Care', 'Shampoo, soap, hand soap, shaving cream'),
 ('Frozen', 'Waffles, Vegetables, Ice Cream'), 
 ('Cleaners', 'Laundry Detergent, Dishwashing Liquid/Detergent');
 
+insert into customers (customername, contactname, address, city, postalcode, country)
+values ('Google', 'Larry Page', '1600 Amphitheatre Pkwy', 'Mountain View', '94043', 'USA'),
+('Apple', 'Steve Jobs', 'Infinite Loop', 'Cupertino', '95014', 'USA'),
+('Amazon', 'Jeff Bezos', '207 Boren Ave N', 'Seattle', '98109', 'USA');
+        
