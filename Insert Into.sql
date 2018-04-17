22a2fad306db3ffc2df68b8dd62f5d960af72d3e
Bryant Truong <Benten1350@yahoo.com>
2018-04-16 22:12:41 -0400

    Create Insert Into.sql

---
+++ Insert Into.sql
@@ -0,0 +1,5 @@
+insert into categories (categoryname, description)
+values ('Personal Care', 'Shampoo, soap, hand soap, shaving cream'), 
+('Frozen', 'Waffles, Vegetables, Ice Cream'), 
+('Cleaners', 'Laundry Detergent, Dishwashing Liquid/Detergent');
+
